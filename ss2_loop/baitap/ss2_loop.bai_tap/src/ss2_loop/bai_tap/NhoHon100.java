dbfe12903144b833a0d1360b66e1831989508155
Minhphuong1609 <126854383+Minhphuong1609@users.noreply.github.com>
2024-12-04 09:15:04 +0700

    Add files via upload

---
+++ ss2_loop/baitap/ss2_loop.bai_tap/src/ss2_loop/bai_tap/NhoHon100.java
@@ -0,0 +1,21 @@
+package ss2_loop.bai_tap;
+
+public class NhoHon100 {
+    public static void main(String[] args) {
+        int number = 2;
+        System.out.println(" số nguyên tố đầu tiền nhỏ hơn 100 là: ");
+        while (number < 100) {
+            boolean isPrime = true;
+            for (int i = 2; i <= Math.sqrt(number); i++) {
+                if (number % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                System.out.println(number);
+            }
+            number++;
+        }
+    }
+}
