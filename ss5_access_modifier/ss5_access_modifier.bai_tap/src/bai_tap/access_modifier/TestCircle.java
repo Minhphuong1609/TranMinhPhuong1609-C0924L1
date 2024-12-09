789c02e6933c34dbd7ebeff14992227f80b0518a
Minhphuong1609 <126854383+Minhphuong1609@users.noreply.github.com>
2024-12-09 11:29:54 +0700

    Add files via upload

---
+++ ss5_access_modifier/ss5_access_modifier.bai_tap/src/bai_tap/access_modifier/TestCircle.java
@@ -0,0 +1,9 @@
+package bai_tap.access_modifier;
+
+public class TestCircle {
+    public static void main(String[] args) {
+        Circle circle = new Circle();
+        System.out.println("Color of circle is " + circle.getColor());
+        System.out.println("Radius of circle is " + circle.getRadius());
+    }
+}
