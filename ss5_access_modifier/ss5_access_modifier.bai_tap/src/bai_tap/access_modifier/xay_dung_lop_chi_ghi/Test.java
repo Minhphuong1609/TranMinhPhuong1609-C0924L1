29ba8617947f03e55dcf20cd7f8a000b8e4b9b8f
Minhphuong1609 <126854383+Minhphuong1609@users.noreply.github.com>
2024-12-11 18:27:06 +0700

    Add files via upload

---
+++ ss5_access_modifier/ss5_access_modifier.bai_tap/src/bai_tap/access_modifier/xay_dung_lop_chi_ghi/Test.java
@@ -0,0 +1,18 @@
+package xay_dung_lop_chi_ghi;
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Student student1 = new Student();
+        System.out.println("----Tên và lớp trước khi đổi----");
+        System.out.println(student1.toString());
+        System.out.println("---Đổi Tên----");
+        student1.setName(in.nextLine());
+        System.out.println("----Đổi Lớp----");
+        student1.setClasses(in.nextLine());
+        System.out.println("----Tên và lớp sau khi đổi----");
+        System.out.println(student1.toString());
+    }
+}
